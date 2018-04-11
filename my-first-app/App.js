4f39d1043991bcf1b4b8832ecfe562549c4b0178
Chase McElroy <ichasem@gmail.com>
2018-04-11 16:47:40 -0700

    created a textbox, button and list

--- my-first-app/App.js
+++ my-first-app/App.js
@@ -1,23 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    state = {
+        placeName: '',
+        places: []
+    };
+
+    placeNameChangedHandler = val => {
+        this.setState({
+            placeName: val
+        });
+    };
+
+    placeSubmitHandler = () => {
+        if (this.state.placeName.trim() === "") {
+            return;
+        }
+        this.setState(prevState => {
+            return {
+                places: prevState.places.concat(prevState.placeName)
+            };
+        });
+    };
+
+    render() {
+        const placeOutput = this.state.places.map((place, i) => (
+            <Text key={i}>{place}</Text>
+        ));
+        return (
+            <View style={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.placeInput}
+                        placeholder="An awesome place..."
+                        value={this.state.placeName}
+                        onChangeText={this.placeNameChangedHandler}
+                    />
+                    <Button
+                        title={"Add"}
+                        style={styles.placeButton}
+                        onPress={this.placeSubmitHandler}
+                    />
+                </View>
+                <View>
+                    {placeOutput}
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 26,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    inputContainer: {
+       // flex: 1,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    placeInput: {
+        width: "70%"
+    },
+    placeButton: {
+        width: "30%"
+    }
 });
